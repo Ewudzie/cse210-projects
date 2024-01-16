a55a0ab46d8e71447239b85e6f5dbbde5f84670f
Ewudzie <cojcoldsgh@yahoo.co.uk>
2024-01-16 13:02:56 +0000

    Added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,12 +6,14 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
 
+        // Ask user for thier name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
+        // Display the user's name in full
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
