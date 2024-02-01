fdf24d304e5cc8d43107b5b7e73d8dd1dccad2d4
Ewudzie <cojcoldsgh@yahoo.co.uk>
2024-02-01 12:56:47 +0000

    update for double verses

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -10,19 +10,22 @@ class Program
 
 
         Reference referencescript = new Reference("John", 3, 16);
-        string text = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+        string text1 = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+
+        Reference referenceScripture2 = new Reference("Proverb", 3, 5, 6);
+        //string text2 = "Trust in the Lord with all thine heart; and lean not unto thine cown understanding.In all thy ways acknowledge him, and he shall direct thy paths.";
         //string book = referencescript.SetBook(b);
         //int chapter = referencescript.SetChapter();
         
     
-        Scripture scriptureRef = new Scripture(referencescript, text);
+        Scripture scriptureRef = new Scripture(referencescript, text1);
        
-        Console.WriteLine($"{referencescript.GetDisplayText()}: {text}");
+        Console.WriteLine($"{referencescript.GetDisplayText()}: {text1}");
         Console.WriteLine();
 
-        Console.Write("Press Enter to continue or type 'quit' to finish. ");
+        Console.WriteLine("Press Enter to continue or type 'quit' to finish: ");
         string userEnter =  Console.ReadLine();
-        //string userQuit = "quit";
+        //string userQuit = "";
 
 
        if (userEnter == Console.ReadLine())
@@ -31,7 +34,7 @@ class Program
             Console.WriteLine("___");
 
         }
-        else 
+        else if(userEnter == "quit")
         {
             Console.Write("Thank you playing the scripture memory game.");
         }
