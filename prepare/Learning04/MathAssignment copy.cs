1d156c547fd43003fe3a2011d82f737d5317ca50
Ewudzie <cojcoldsgh@yahoo.co.uk>
2024-02-02 23:44:01 +0000

    Creating lesson 4 Assignment

---
+++ prepare/Learning04/MathAssignment copy.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class MathAssignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment (string textbookSection, string problems)
+    {
+
+
+    }
+
+        public string GetHomework()
+        {
+            return "";
+        }
+
+        
+    
+}
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class WritingAssignment
+{
+    private string _title;
+    
+
+    public WritingAssignment (string title)
+    {
+
+
+    }
+
+        public string GetWritingInformation()
+        {
+            return "";
+        }
+
+        
+    
+}
