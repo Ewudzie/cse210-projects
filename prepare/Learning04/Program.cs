f5f183610f4d436eaa2d812eb163d42e4c9cbebe
Ewudzie <cojcoldsgh@yahoo.co.uk>
2024-02-03 21:56:23 +0000

    run program in main program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -10,5 +10,20 @@ class Program
         //assignment.GetSummary();
 
         Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+
+        MathAssignment mathHomework = new MathAssignment("Mary Arthur", "Fractions", "7.3", "8-19" );
+
+        Console.WriteLine(mathHomework.GetSummary());
+
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writeAssignment = new WritingAssignment("Jude Rose", "European History", "The Causes of World War II");
+
+        Console.WriteLine(writeAssignment.GetSummary());
+        Console.WriteLine(writeAssignment.GetWritingInformation());
     }
 }
